e6ff6dc30641418c9d3c597fa333658c61d79287
Arras <nikolai.arras@accenture.com>
2023-02-22 17:53:27 +0100

    chnaged build spec to windows

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function helloWorld() {
+    const element = document.getElementById("hello-world");
+    if (element) {
+        element.textContent = "Hello, World!";
+    }
+}
+helloWorld();
+//# sourceMappingURL=index.js.map
